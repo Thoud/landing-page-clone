9a7fc3b1100ec776d467e2762f29aff89d5ae361
Robert Miron <73788820+Thoud@users.noreply.github.com>
2021-02-01 15:37:24 +0100

    Get all images and determine colors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 
+const white = css`
+  color: #fafafa;
+`;
+
+const grayWhite = css`
+  color: #f8f9fa;
+`;
+
+const veryLightBlue = css`
+  color: #e6eafc;
+`;
+
+const lightBlue = css`
+  color: #546ee8;
+`;
+
+const darkBlue = css`
+  color: #243141;
+`;
+
 export default function App() {
   return <div />;
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { css, Global } from '@emotion/react';
 import App from './App';
+
+const globalStyles = css`
+  body {
+    margin: 0;
+    width: 100vw;
+    height: 100vh;
+  }
+
+  body,
+  button {
+    font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  #root {
+    display: grid;
+  }
+`;
 
 ReactDOM.render(
   <React.StrictMode>
+    <Global styles={globalStyles} />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
